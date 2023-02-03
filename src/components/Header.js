1ba85b1949d9ad584eeb8e1244be1ac26bfb1777
thiago-papim <thiago.fourp@gmail.com>
2023-02-03 17:57:43 -0300

    Requisito 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import './style.css';
+import '../images/som.png';
 
 class Header extends React.Component {
   state = {
@@ -23,10 +25,11 @@ class Header extends React.Component {
     const nameLogin = <p data-testid="header-user-name">{ `Usuário: ${name}` }</p>;
     return (
       <header data-testid="header-component">
-        <div>
+        <div className="usuario">
+          <img src="https://seeklogo.com/images/S/sua-musica-logo-FB924E2959-seeklogo.com.png" alt="som" />
           { load ? <Loading /> : nameLogin }
         </div>
-        <div>
+        <div className="links">
           <Link
             to="/search"
             data-testid="link-to-search"
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,47 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    btnPesquisa: true,
+    search: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value }, this.validation(value));
+  };
+
+  validation = (valor) => {
+    const numberThree = 2;
+    if (valor.length >= numberThree) {
+      this.setState({ btnPesquisa: '' });
+    } else { this.setState({ btnPesquisa: true }); }
+  };
+
   render() {
+    const { search, btnPesquisa } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Search</p>
+        <form>
+          <label htmlFor="search">
+            <input
+              value={ search }
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Nome da banda ou artista"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <input
+            type="button"
+            data-testid="search-artist-button"
+            value="Procurar"
+            disabled={ btnPesquisa }
+          />
+        </form>
       </div>
     );
   }
