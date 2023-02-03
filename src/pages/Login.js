6f69c55bed81ee4c94fe1816cc5f71c945cc01af
thiago-papim <thiago.fourp@gmail.com>
2023-02-03 16:35:07 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    btnDisabled: true,
+    load: false,
+  };
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value }, this.validation(value));
+  };
+
+  validation = (valor) => {
+    const numberThree = 3;
+    if (valor.length >= numberThree) {
+      this.setState({ btnDisabled: '' });
+    } else { this.setState({ btnDisabled: true }); }
+  };
+
   render() {
+    const { name, btnDisabled, load } = this.state;
+    const { history } = this.props;
     return (
       <div data-testid="page-login">
-        <p>oi</p>
+        <form>
+          <label htmlFor="name">
+            Digite seu nome
+            <input
+              type="text"
+              id="name"
+              name="name"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+              value={ name }
+            />
+          </label>
+          <input
+            type="button"
+            data-testid="login-submit-button"
+            value="Entrar"
+            disabled={ btnDisabled }
+            onClick={ async () => {
+              this.setState({ load: true });
+              await createUser({ name });
+              history.push('/search');
+            } }
+          />
+        </form>
+        <h1>{ load ? <Loading /> : '' }</h1>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Login;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,7 +4,7 @@ class Search extends React.Component {
   render() {
     return (
       <div data-testid="page-search">
-        <p>oi</p>
+        <p>Search</p>
       </div>
     );
   }
