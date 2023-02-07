de13ce561e5bd8b8b4bde66641bcc681054b6dcb
thiago-papim <thiago.fourp@gmail.com>
2023-02-07 18:12:15 -0300

    recuperando

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -25,8 +25,8 @@ class Profile extends React.Component {
     const perfil = (
       <div className="divProfile">
         <img
-          src={ image || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png' }
-          alt=""
+          src={ image }
+          alt={ `Foto de ${name}` }
           data-testid="profile-image"
         />
         <p>{name}</p>
